49d773d1540b0bc06a32496f13400ff3ce4459d1
Ricardo Cesar <ol.rcesar@gmail.com>
2024-10-20 13:24:38 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -20,8 +20,8 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888-8888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "97777-7777", "123456");
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888-8888", "123457");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "97777-7777", "123458");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+//@Repository		//opcional, pois o UserRepository já está herdando do JPARepository, que possui registro como componente do Spring
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
