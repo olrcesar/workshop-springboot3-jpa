15398ecbaff639fee109aabcd50a1c0e60e6115f
Ricardo Cesar <ol.rcesar@gmail.com>
2024-10-23 15:37:29 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem oi : items) {
+			sum += oi.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
